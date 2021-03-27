7594c110149be1dcb610f7f834709206ebb22e1c
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2021-03-26 22:36:23 -0300

    Salvando os anúncios no parser

--- lib/repositories/ad_repository.dart
+++ lib/repositories/ad_repository.dart
@@ -4,11 +4,56 @@ import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:xlo_app/models/ad.dart';
 import 'package:xlo_app/repositories/parse_errors.dart';
 import 'package:path/path.dart' as path;
+import 'package:xlo_app/repositories/table_keys.dart';
 
 class AdRepository {
+
   Future<void> save(Ad ad) async {
-    final parseImages = await saveImages(ad.images);
+    try {
+      final parseImages = await saveImages(ad.images);
+
+      final parseUser = ParseUser('', '', '')..set(keyUserId, ad.user.id);
+
+      final adObject = ParseObject(keyAdTable);
+
+      if (ad.id != null) adObject.objectId = ad.id;
+
+      final parseAcl = ParseACL(owner: parseUser);
+      parseAcl.setPublicReadAccess(allowed: true);
+      parseAcl.setPublicWriteAccess(allowed: false);
+      adObject.setACL(parseAcl);
+
+      adObject.set<String>(keyAdTitle, ad.title);
+      adObject.set<String>(keyAdDescription, ad.description);
+      adObject.set<bool>(keyAdHidePhone, ad.hidePhone);
+      adObject.set<num>(keyAdPrice, ad.price);
+      adObject.set<int>(keyAdStatus, ad.status.index);
+
+      adObject.set<String>(keyAdDistrict, ad.address.district);
+      adObject.set<String>(keyAdCity, ad.address.city.name);
+      adObject.set<String>(keyAdFederativeUnit, ad.address.uf.initials);
+      adObject.set<String>(keyAdPostalCode, ad.address.cep);
+
+      adObject.set<List<ParseFile>>(keyAdImages, parseImages);
+
+      adObject.set<ParseUser>(keyAdOwner, parseUser);
+
+      adObject.set<ParseObject>(keyAdCategory,
+          ParseObject(keyCategoryTable)..set(keyCategoryId, ad.category.id));
+
+      final response = await adObject.save();
+
+      if (!response.success) {
+        return Future.error(ParseErrors.getDescription(response.error.code));
+      }
+    } catch (e) {
+      print(e);
+      return Future.error('Falha ao salvar anúncio');
+    }
   }
+
+
+
 
   Future<List<ParseFile>> saveImages(List images) async {
     final parseImages = <ParseFile>[];
