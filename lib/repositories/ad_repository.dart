0b853471d444db181e2a016fe5520bc85d48e805
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2021-03-26 21:28:27 -0300

    Salvando as imagens

--- lib/repositories/ad_repository.dart
+++ lib/repositories/ad_repository.dart
@@ -1,9 +1,39 @@
+import 'dart:io';
+
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:xlo_app/models/ad.dart';
+import 'package:xlo_app/repositories/parse_errors.dart';
+import 'package:path/path.dart' as path;
 
 class AdRepository {
-
-  void save(Ad ad) {
-    //salvar os dados do anuncio
+  Future<void> save(Ad ad) async {
+    final parseImages = await saveImages(ad.images);
   }
 
+  Future<List<ParseFile>> saveImages(List images) async {
+    final parseImages = <ParseFile>[];
+
+    try {
+      for (final image in images) {
+        if (image is File) {
+          final parseFile = ParseFile(image, name: path.basename(image.path));
+          final response = await parseFile.save();
+          if (!response.success) {
+            return Future.error(
+                ParseErrors.getDescription(response.error.code));
+          }
+          parseImages.add(parseFile);
+        } else {
+          final parseFile = ParseFile(null);
+          parseFile.name = path.basename(image);
+          parseFile.url = image;
+          parseImages.add(parseFile);
+        }
+      }
+
+      return parseImages;
+    } catch (e) {
+      return Future.error('Falha ao salvar imagens');
+    }
+  }
 }
